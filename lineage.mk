493ae0483ddfb457f73f776e1f0395a7cdd080ac
Kevin F. Haggerty <kevin.f.haggerty@gmail.com>
2016-11-12 15:48:17 -0700

    scale: Set TARGET_BOOTANIMATION_HALF_RES to true
    * No apparent resolution difference
    * Much smoother animation
    * Much faster boot
    * What's not to like?
    
    Change-Id: I2119fe826146a3103da50b4862ad88f5950a97ec

--- lineage.mk
+++ lineage.mk
@@ -25,6 +25,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
+TARGET_BOOTANIMATION_HALF_RES := true
 
 # Device identifications
 PRODUCT_DEVICE := scale
